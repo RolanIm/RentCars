2721d0314b82c7f6cdfe5a1f32a290c0cb782ad0
Rolan_Imangulov <imangulov.roman2015@gmail.com>
2023-07-27 16:21:41 +0500

    changed gitingore

--- RentCars/settings.py
+++ RentCars/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from secret import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,12 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8p^@c4ovnvzf!uvru&f01db7sxq4cutlt@c0-hbv69)=!nuaet'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
